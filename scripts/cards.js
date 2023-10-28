79fb89b62394bea2169b3b07fffc79b7cbd6e654
Vyacheslav Vodolaz <vodolaz1997@gmail.com>
2023-10-28 22:02:42 +0300

    Исправлено.

--- scripts/cards.js
+++ scripts/cards.js
@@ -27,5 +27,6 @@ const initialCards = [
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "Река и горы",
     }
 ];
